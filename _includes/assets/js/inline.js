ccf74e8d2a683755116859bf30ea131e4e257eb2
Md. Emran Hossen <75485859+emrantusho@users.noreply.github.com>
2025-09-19 16:01:50 +0600

    Update inline.js

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,7 +1,1 @@
-{# Conditionally load and inline solar system JavaScript for the homepage #}
-{% if section == "home" %}
-  {% set js %}
-    {% include "assets/js/solar-system.js" %}
-  {% endset %}
-  <script>{{ js | jsmin | safe }}</script>
-{% endif %}// Add your inline JS here
+
